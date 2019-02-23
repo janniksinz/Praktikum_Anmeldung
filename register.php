f039c2ad1ad8716ceb09b243f1f7b16fc6cc5243
janniksinz <j.sinz@gmx.de>
2019-02-23 19:18:43 +0100

    initial commit

---
+++ register.php
@@ -0,0 +1,37 @@
+
+           
+<?php
+    
+    
+?>
+          
+           <div class="uk-section uk-container uk-text-left">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+                
+                <!--Form-->       
+                        <form class="uk-form-stacked js.login" method="get">
+                            <!--Email Holder-->
+                            <div class="uk-margin">
+                                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                                <div class="uk-form-controls input_form">
+                                    <input class="uk-input" id="form-stacked-text" type="email" required="required" placeholder="email@email.com">
+                                </div>
+                            </div>
+                            <!--Password Holder-->
+                            <div class="uk-margin">
+                                <label class="uk-form-label" for="form-stacked-text">Passord</label>
+                                <div class="uk-form-controls input_form">
+                                    <input class="uk-input" id="form-stacked-text" type="password" required="required" placeholder="Your Password">
+                                </div>
+                            </div>
+                            <!--Submit Button-->
+                            <div class="uk-margin">
+                                <div class="uk-form-controls">
+                                    <input class="uk-input" id="form-stacked-text" type="submit" value="Register">
+                                </div>
+                            </div>
+
+                        </form>
+                
+            </div>
+        </div>
