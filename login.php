73be31a65a6a315b66f4f2f7c76d654093356ef2
janniksinz <j.sinz@gmx.de>
2019-02-23 13:55:08 +0100

    4Commit_Login_Form_and_Style

--- login.php
+++ login.php
@@ -15,6 +15,7 @@
 
     <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
     <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="style_login.css">
 </head>
 
 <body>
@@ -31,7 +32,7 @@
        <nav class="">
            <ul>
                <li><a href="index.html">Home</a></li>
-               <li><a href="#info">Infos</a></li>
+               <!--<li><a href="index.html">Infos</a></li>-->
                <li><a href="login.php">Anmelden</a></li>
                <li class="libottom"><a href="#">ehemalige Praktika</a></li>              
            </ul>
@@ -46,18 +47,19 @@
     <div class="pimg1">
         <div class="ptext">
             <span class="border">
-                Praktikumsanmeldung
+                Login
             </span>
         </div>
     </div>
     
-    
+    <!--
     <section class="section section1 section-light" id="info">
-        <h2>Infos</h2>
+        <h2>Anmelden</h2>
         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam atque debitis id earum pariatur. Sunt placeat quibusdam iure rerum nisi quas, deserunt doloribus expedita. Temporibus nam molestiae sint voluptate eius dolorum eum aspernatur fuga delectus numquam sit amet, maiores consequatur aut molestias laudantium voluptatum esse, asperiores qui ratione reprehenderit ducimus? Illum deserunt minima quisquam aliquid obcaecati quia atque placeat. Harum voluptatum, fuga ratione dolor expedita repellendus accusamus vero eos odio. Molestiae saepe sed quia quasi corrupti, quo optio architecto culpa.</p>
        <!-- <a href="login.php">Login</a>-->
-    </section>
-    
+    <!--------------------
+    </section> 
+    ------------------->
     <!--next Parallax Image-->
 
    <!----------------------------------
@@ -109,7 +111,47 @@
     -------------------->
     
     <section class="section section-dark">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam delectus a necessitatibus, impedit, unde nobis ratione atque consequuntur sit voluptatibus.
+        <h2>Registrieren</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ullam delectus a necessitatibus, impedit, unde nobis ratione atque consequuntur sit voluptatibus.</p>
+        
+        <!--Login Form Section-->
+        
+        <div class="uk-section uk-container uk-text-left">
+            <div class="uk-grid uk-child-width-1-3@s uk-child-width-1-1" uk-grid="">
+                
+                <!--Form-->       
+                        <form class="uk-form-stacked">
+                            <!--Email Holder-->
+                            <div class="uk-margin">
+                                <label class="uk-form-label" for="form-stacked-text">Email</label>
+                                <div class="uk-form-controls input_form">
+                                    <input class="uk-input" id="form-stacked-text" type="email" placeholder="email@email.com">
+                                </div>
+                            </div>
+                            <!--Password Holder-->
+                            <div class="uk-margin">
+                                <label class="uk-form-label" for="form-stacked-text">Passord</label>
+                                <div class="uk-form-controls input_form">
+                                    <input class="uk-input" id="form-stacked-text" type="password" placeholder="Your Password">
+                                </div>
+                            </div>
+                            <!--Submit Button-->
+                            <div class="uk-margin">
+                                <div class="uk-form-controls">
+                                    <input class="uk-input" id="form-stacked-text" type="submit" value="Register" >
+                                </div>
+                            </div>
+
+                        </form>
+                
+            </div>
+        </div>
+        
+        
+    
+        
+        
+        
     </section>
     
     
