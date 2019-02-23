85ba1c344311cd0ed9f8820e363124925f59bebe
janniksinz <j.sinz@gmx.de>
2019-02-23 19:40:36 +0100

    initial commit

---
+++ inc/config.php
@@ -0,0 +1,22 @@
+<?php
+    /*If there is no '__CONFIG__' defined, do not loead this file*/
+    if (!defined('__CONFIG__'))
+    {
+        exit('You do not have a config file');
+    }    
+
+    define('__ALLOW_FOOTER__', true);
+        
+    /*Config down here*/
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+?>
---
+++ inc/footer.php
@@ -0,0 +1,14 @@
+  <?php  
+
+
+    if (!defined('__ALLOW_FOOTER__'))
+    {
+        exit('You do not have a config file');
+    }    
+
+
+    ?>
+
+    
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/uikit/3.0.3/js/uikit-icons.min.js"></script>
