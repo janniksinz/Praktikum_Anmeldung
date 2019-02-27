3b0ea0e2109f3e35618b340a7509d62e71c54df6
janniksinz <j.sinz@outlook.com>
2019-02-27 19:55:00 +0100

    8Commit LoginSite and Form

--- Database/01-setup.sql
+++ Database/01-setup.sql
@@ -1,23 +1,12 @@
--- phpMyAdmin SQL Dump
--- version 4.5.4.1deb2ubuntu2
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Aug 27, 2017 at 12:35 PM
--- Server version: 5.7.19-0ubuntu0.16.04.1
--- PHP Version: 7.0.22-0ubuntu0.16.04.1
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
 
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
 --
--- Database: `login_course`
+
 --
 CREATE DATABASE IF NOT EXISTS `praktika` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 USE `praktika`;
@@ -25,7 +14,7 @@ USE `praktika`;
 -- --------------------------------------------------------
 
 --
--- Table structure for table `users`
+
 --
 
 DROP TABLE IF EXISTS `users`;
@@ -61,6 +50,3 @@ ALTER TABLE `users`
 --
 ALTER TABLE `users`
   MODIFY `user_id` int(5) UNSIGNED NOT NULL AUTO_INCREMENT COMMENT 'User Id';
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
