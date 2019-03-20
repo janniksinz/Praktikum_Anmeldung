dfe378d7b223b8949b69336779e03f0862f37126
janniksinz5 <janniksinz5@gmx.de>
2019-03-20 07:32:21 -0400

    11Commit FormPOST

--- admin.php
+++ admin.php
@@ -12,7 +12,7 @@
         <div class="row">
             <div class="col-md-6 login-left">
                 <h2>Login Here:</h2>
-                <form action="Assets/validation.php">
+                <form action="Assets/validation.php" method="POST">
                     <div class="form-group">
                         <label>Email</label>
                         <input type="email" name="email" id="email" class="form-control">
@@ -32,7 +32,7 @@
         
             <div class="col-md-6 login-right">
                 <h2>Register Here:</h2>
-                <form action="Assets/registration.php">
+                <form action="Assets/registration.php" method="POST">
                     <div class="form-group">
                         <label>Email</label>
                         <input type="email" name="email" id="email" class="form-control">
