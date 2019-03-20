4444941ab9f9d16b0216dbb939d25d0a7b2f16f4
janniksinz <j.sinz@outlook.com>
2019-03-20 12:43:05 +0100

    11Commit MSQLI Adnfrage erfolgreich

--- Assets/registration.php
+++ Assets/registration.php
@@ -1,17 +1,19 @@
 <?php
+
+define ('__CONFIG__',true);
 require_once "../inc/config.php";    
 session_start();
 
 $con = mysqli_connect('localhost', 'root', '');
-$
+
 
 $u_email = $_POST['email'];
 $u_pw    = $_POST['pw'];
     
-/*Try to get the same user from the DB*/    
+/*Try to get the same user from the DB*/   
 $s = " SELECT * FROM users WHERE name = '$u_email'";
-/*give userdata to DB*/    
-$result = mysqli_query($object, $s);
+/*give userdata to DB*/  
+$result = mysqli_query($con, $s);
 
 $num = mysqli_num_rows($result);
 /*If email is already taken... output... / Else insert into users*/
@@ -21,8 +23,9 @@ if ($num == 1)
 } else {
     $reg = " INSERT INTO users(email, password) 
             VALUES ('$u_email', '$u_pw')";
-    mysqli_query($object, $reg);
-    echo 'Registration successful';
+    mysqli_query($con, $reg);
+    echo 'Registration successful<br>';
 }
+echo 'HI';
     
 ?>
